303e4c2c0922bdb6a521e635219aa47f7abfefa1
Ninjaprawn <george.r.dan@gmail.com>
2016-03-28 18:25:44 +1100

    Initial Commit

---
+++ iFW/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iFW
+//
+//  Created by George Dan on 28/03/2016.
+//  Copyright © 2016 George Dan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
