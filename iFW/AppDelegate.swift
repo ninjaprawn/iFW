bebde745a269f2bc7dd86544a27d0c69e0e26a20
Ninjaprawn <george.r.dan@gmail.com>
2016-04-08 07:47:00 +1000

    Testing blue nav bar

--- iFW/AppDelegate.swift
+++ iFW/AppDelegate.swift
@@ -13,9 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
 
-
 	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 		// Override point for customization after application launch.
+		
+		UINavigationBar.appearance().barTintColor = UIColor.blueColor()
+		
 		return true
 	}
 
