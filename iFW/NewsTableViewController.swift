8eafe4d8b2c163b973e858f329816f69cb462090
Ninjaprawn <george.r.dan@gmail.com>
2016-04-07 17:44:08 +1000

    Getting data from ipsw.me, displaying it in cool tableview

--- iFW/NewsTableViewController.swift
+++ iFW/NewsTableViewController.swift
@@ -10,14 +10,25 @@ import UIKit
 
 class NewsTableViewController: UITableViewController {
 
+	var items = [RSSItem]()
+	
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+		// TODO: Rewrite RSSParser for just this case. Sideproject - Rewrite SwiftRSS and maintain it
+		let request: NSURLRequest = NSURLRequest(URL: NSURL(string: "https://ipsw.me/updates.rss")!)
+		RSSParser.parseFeedForRequest(request, callback: { (feed, error) in
+			guard let feed = feed else {
+				print(error)
+				return
+			}
+			
+			self.items = feed.items
+			dispatch_sync(dispatch_get_main_queue(), {
+				self.tableView.reloadSections(NSIndexSet(index: 0), withRowAnimation: .Automatic)
+			})
+			
+		})
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,62 +40,33 @@ class NewsTableViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return items.count
     }
 
-    /*
+	
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("newsCell", forIndexPath: indexPath)
 
-        // Configure the cell...
+		let titleLabel = cell.viewWithTag(1000) as! UILabel
+		let dateLabel = cell.viewWithTag(1001) as! UILabel
+		
+		titleLabel.text = items[indexPath.row].title
+		
+		let dateFormatter = NSDateFormatter()
+		// TODO: Localize date (format + timezone)
+		dateFormatter.dateFormat = "dd/MM/yyyy"
+		
+		if let date = items[indexPath.row].pubDate {
+			dateLabel.text = dateFormatter.stringFromDate(date)
+		} else {
+			dateLabel.text = "Date not found"
+		}
 
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+	override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+		tableView.deselectRowAtIndexPath(indexPath, animated: true)
+	}
 
 }
