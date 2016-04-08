25378ed953a39a517c87cff3ab140ff7fa2679d1
Ninjaprawn <george.r.dan@gmail.com>
2016-04-08 21:04:04 +1000

    Dynamic title for firmwares (not confirmed)

--- iFW/FirmwareTableViewController.swift
+++ iFW/FirmwareTableViewController.swift
@@ -15,11 +15,7 @@ class FirmwareTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        self.title = device.deviceName
     }
 
     override func didReceiveMemoryWarning() {
